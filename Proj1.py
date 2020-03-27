ab214d2b22b1b18bf2cb4b20b70c95b39eaa1677
garrettyzk <garrettyzk@gmail.com>
2020-03-27 11:03:50 -0700

    hello

--- Proj1.py
+++ Proj1.py
@@ -5,24 +5,34 @@ def display_board(board):
     print(board[4]+'|'+board[5]+'|'+board[6])
     print('- - -')
     print(board[7]+'|'+board[8]+'|'+board[9])
+def position(player):
+    if ' ' not in user_board:
+        return None
+    move=int(input('What position would you like to go to?'))
+    while move>9 or move<1 or move is '':
+        move=int(input('What position would you like to go to?'))
+    while user_board[move] != ' ':
+        move=int(input('What position would you like to go to?'))
+    user_board[move]=player
+    return display_board(user_board)
 
+example_board=['#','1','2','3','4','5','6','7','8','9']
 user_board = ['#',' ',' ',' ',' ',' ',' ',' ',' ',' ']
 
-player1=input('Player 1! Please choose X or O')
+#game start
+player1=input('Player 1 Please choose X or O')
 while player1 not in 'OXxo':
     player1=input('Please choose X or O')
-print('{1}{2}{3}\n{4}{5}{6}\n{7}{8}{9}')
 
-if player1 is 'X':
-    player2 is 'O'
-elif player1 is 'O':
-    player2 is 'X'
+display_board(example_board)
 
-move=int(input('What position would you like to go to?'))
-while move>9 or move<1 or move is '':
-    move=int(input('What position would you like to go to?'))
+player2=None
+if player1 in 'Xx':
+    player2 = 'O'
+else:
+    player2 = 'X'
+player1=player1.upper()
 
-def input1(example):
-    player1=user_board[example]
-    return display_board(user_board)
-print(input1(move))
+while ' ' in user_board:
+    position(player1)
+    position(player2)
